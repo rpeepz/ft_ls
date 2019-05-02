3768b26572f98232379901dbdef0b3179f27ced9
Robert Papagna <rpapagna@e1z2r8p23.42.us.org>
2019-05-02 02:34:18 -0700

    small progress

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 20:04:56 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/05/02 00:25:39 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/05/02 02:33:51 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,23 @@
 # include <time.h>
 # include <pwd.h>
 # include <grp.h>
+
+/*
+**STRUCTURES
+*/
+
+/*
+**	MANDATORY FLAGS
+**	-R	Recursively list subdirectories encountered.
+**	-a	Include directory entries whose names begin with a dot (.).
+**	-l	List in long format. If the output is to a terminal, a total sum
+**		for all the file sizes is output on a line before the long listing.
+**	-r	Reverse the order of the sort to get reverse lexicographical
+**		order or the oldest entries first
+**	-t	Sort by time modified (most recently modified first) before sorting
+**		the operands by lexicographical order
+**	OPTIONS FLAGS
+**	-f	Output is not sorted. This option turns on the -a option.
+*/
 
 #endif
