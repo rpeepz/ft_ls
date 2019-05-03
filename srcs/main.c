2fce750fb72fe4ec45a04cb2cd0169c9b7e23ab2
Robert Papagna <rpapagna@e1z2r8p25.42.us.org>
2019-05-03 12:36:00 -0700

    working flag reception and error msgs

--- srcs/main.c
+++ srcs/main.c
@@ -6,43 +6,58 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 21:48:57 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/05/02 03:13:35 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/05/03 12:33:20 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
 
-static char		get_flags(int param_count, char **str, char flags)
+static int		copy_print(char *msg, char *attach, char *buf, int len)
 {
-	while (param_count-- > 0 && *str && **str == '-')
+	ft_strncpy(buf, msg, (len = LEN(msg)));
+	ft_strncpy(buf + len++, attach, 1);
+	ft_strncpy(buf + len++, "\n", 1);
+	ft_putnstr(buf, len);
+	return (1);
+}
+
+static char		get_flags(int index, int param_count, char **str, char mask)
+{
+	char	buf[40];
+
+	while ((str++) && index++ < param_count && **str == '-' && ((*str)++))
 	{
-		while ((*str)++)
+		if (IS_FLAG(**str))
+			while (IS_FLAG(**str))
+			{
+				IF_THEN((**str == 'R' && (*str)++), mask |= 1UL << 0);
+				IF_THEN((**str == 'a' && (*str)++), mask |= 1UL << 1);
+				IF_THEN((**str == 'l' && (*str)++), mask |= 1UL << 2);
+				IF_THEN((**str == 'r' && (*str)++), mask |= 1UL << 3);
+				IF_THEN((**str == 't' && (*str)++), mask |= 1UL << 4);
+			}
+		else if (ISNT_FLAG(**str))
 		{
-			if (**str == 'R' && (*str)++)
-				flags += 2;//set bit for 'R' flag
-			if (**str == 'a' && (*str)++)
-				flags += 2;//set bit for 'a' flag
-			if (**str == 'l' && (*str)++)
-				flags += 2;//set bit for 'l' flag
-			if (**str == 'r' && (*str)++)
-				flags += 2;//set bit got 'r' flag
-			if (**str == 't' && (*str)++)
-				flags += 2;//set bit for 't' flag
-			else if (**str != 'R' || **str != 'a' || **str != 'l' ||
-					**str != 'r' || **str != 't' || **str == '\0')
-				break ;//if flag is invalid
+			IF_RETURN(!**str, mask);
+			IF_BREAK(**str == '-' && !(*(*str + 1)));
+			if (**str == '-' && *(*str + 1))
+				ft_putstr("2ft_ls: illigal option -- -\n");
+			else if (ISNT_FLAG(**str))
+				copy_print("1ft_ls: illigal option -- ", *str, buf, 0);
+			return (1UL << 7);
 		}
-		str++;
 	}
-	return (flags);
+	return (mask);
 }
 
 int				main(int ac, char **av)
 {
-	char	flags;
+	t_file	path;
 
-	flags = get_flags(ac - 1, av + 1, 0);
-	ft_printf("%d\n", ac);
-	ft_printf("%c\n", flags + '0');
+	path.flags = get_flags(0, ac - 1, av, 0);
+	ft_printf("ac: %15.4f\n", (float)ac);
+	ft_printf("fl: %#15.6b\n", path.flags);
+	if (path.flags & 1UL << 7)
+		ft_putstr("usage: ./ft_ls [-Ralrt] [file ...]\n");
 	return (0);
 }
