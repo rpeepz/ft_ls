614d6d1db3fc4bade9602ab03911e339bd0daf0c
rpeepz <r.papagna.42@gmail.com>
2019-06-02 01:58:32 -0700

    cleaned up main

--- srcs/main.c
+++ srcs/main.c
@@ -6,22 +6,11 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 21:48:57 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/05/30 17:57:23 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/06/02 01:41:45 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
-
-static int		dispatch_ls_mode(t_file *paths, char flags)
-{
-	if (flags ^ 0x62)
-	{
-		ft_ls(paths, flags);
-		return (0);
-	}
-	else
-		return (dash_onef(paths));
-}
 
 int				main(int ac, char **av)
 {
@@ -32,9 +21,9 @@ int				main(int ac, char **av)
 	if (ac > 1)
 	{
 		if ((flags = get_flags(av, 0, 0, 0)) == '~')
-			IF_RETURN(write(1, "usage: ./ft_ls [-Ralrt] [file ...]\n", 35), 0);
+			IF_RETURN(write(1, USAGE, 37), 1);
 		if (!(paths = get_dirs(av, 1, 0)))
-			IF_RETURN(write(1, "malloc error, yo\n", 17), 0);
+			IF_RETURN(write(1, MALLOC_ERR, 17), 1);
 	}
 	else
 	{
@@ -44,9 +33,9 @@ int				main(int ac, char **av)
 	if (!paths->next)
 		paths->index = 0;
 	else
-		t_file_mergesort(&paths, flags);
-	if (dispatch_ls_mode(paths, flags))
-		write(1, "malloc error, yo\n", 17);
+		t_file_mergesort(&paths, flags, 1);
+	if (ft_ls(paths, flags))
+		write(1, MALLOC_ERR, 17);
 	t_filedel(paths);
 	return (0);
 }
