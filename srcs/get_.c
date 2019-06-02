d712078cae9372d1560b55248739b1ee2fbef82c
Robert Papagna <rpapagna@e1z2r8p21.42.us.org>
2019-06-01 21:36:04 -0700

    remove copy_print add sprintf, adjust masking process

--- srcs/get_.c
+++ srcs/get_.c
@@ -6,52 +6,44 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 01:54:58 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/05/30 05:12:11 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/06/01 21:34:03 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
 
-static int		copy_print(char *buf, char *msg, char attach, int len)
+static void		mask_flag(char *mask, char option)
 {
-	ft_strncpy(buf, msg, (len = LEN(msg)));
-	ft_strncpy(buf + len++, &attach, 1);
-	ft_strncpy(buf + len++, "\n", 1);
-	ft_putnstr(buf, len);
-	return (1);
-}
-
-void			mask_flag(char *mask, char option)
-{
-	if (option == 'R')
+	if (option == '1')
 		*mask |= 1 << 0;
-	else if (option == 'a')
+	else if (option == 'R')
 		*mask |= 1 << 1;
-	else if (option == 'l')
+	else if (option == 'a')
 		*mask |= 1 << 2;
-	else if (option == 'r')
-		*mask |= 1 << 3;
-	else if (option == 't')
-		*mask |= 1 << 4;
-	else if (option == '1')
-		*mask |= 1 << 5;
 	else if (option == 'f')
 	{
-		*mask |= 1 << 6;
-		*mask |= 1 << 1;
+		*mask |= 1 << 3;
+		*mask |= 1 << 2;
 	}
+	else if (option == 'l')
+		*mask |= 1 << 4;
+	else if (option == 'r')
+		*mask |= 1 << 5;
+	else if (option == 't')
+		*mask |= 1 << 6;
 }
 
 char			get_flags(char **av, char bt, int i, int j)
 {
-	char	buf[30];
+	char	buf[PAGESIZE];
 
 	while ((av[++i]) && av[i][0] == '-' && (j = 1))
 	{
 		while (IS_FLAG(av[i][j]) || IS_FLAG2(av[i][j]))
 		{
-			IF_RETURN(av[i][j] == '-' && copy_print(buf,
-			"ft_ls: illigal option -- ", av[i][j], 0), '~');
+			IF_RETURN(av[i][j] == '-' &&
+			ft_sprintf(buf, "%s%c\n", "ft_ls: illigal option -- ", av[i][j]) &&
+			write(1, buf, 27), '~');
 			mask_flag(&bt, av[i][j]);
 			j++;
 		}
@@ -59,9 +51,10 @@ char			get_flags(char **av, char bt, int i, int j)
 		{
 			IF_BREAK(!av[i][j]);
 			IF_BREAK(av[i][j] == '-' && !av[i][j + 1]);
-			IF_RETURN((av[i][j] == '-' && av[i][j + 1] || (NOT_FLAG(av[i][j]) &&
-				NOT_FLAG2(av[i][j]))) && copy_print(buf,
-				"ft_ls: illigal option -- ", av[i][j], 0), '~');
+			IF_RETURN((av[i][j] == '-' && av[i][j + 1] ||
+			(NOT_FLAG(av[i][j]) && NOT_FLAG2(av[i][j]))) &&
+			ft_sprintf(buf, "%s%c\n", "ft_ls: illigal option -- ", av[i][j]) &&
+			write(1, buf, 27), '~');
 		}
 	}
 	return (bt);
