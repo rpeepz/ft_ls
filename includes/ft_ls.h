694759d14c71888d5abb2de7660892a9a5801779
rpeepz <r.papagna.42@gmail.com>
2019-06-02 02:04:16 -0700

    cleaned up header, add new macros

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 20:04:56 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/05/30 17:59:35 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/06/02 01:42:15 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,28 @@
 # include <grp.h>
 
 /*
-**	-------MACROS-------
+**	--------------------------------
+**				MACROS
+**	--------------------------------
 */
 
-# define IS_FLAG(x) (x == 'R' || x == 'a' || x == 'l' || x == 'f')
-# define IS_FLAG2(x) (x == 'r' || x == 't' || x == '1')
-# define NOT_FLAG2(x) (x != 'r' && x != 't' && x != '1')
-# define NOT_FLAG(x) (x != 'R' && x != 'a' && x != 'l' && x != 'f')
+# define USAGE "usage: ./ft_ls [-1Raflrt] [file ...]\n"
+# define MALLOC_ERR "malloc error, yo\n"
+# define IS_FLAG(x) (x == '1' || x == 'R' || x == 'a' || x == 'f')
+# define IS_FLAG2(x) (x == 'l' || x == 'r' || x == 't')
+# define NOT_FLAG(x) (x != '1' && x != 'R' && x != 'a' && x != 'f')
+# define NOT_FLAG2(x) (x != 'l' && x != 'r' && x != 't')
 # define CURRENT_DIR(x) (x[0] == '.' && x[1] == '\0')
 # define PARENT_DIR(x) (x[0] == '.' && x[1] == '.' && x[2] == '\0')
 # define IF_BREAK(x) if(x) {break ;}
 # define IF_THEN_CONTINUE(x, y) if(x) {(y); continue ;}
+# define Y_DIR(t_file) S_ISDIR(t_file->info.st_mode)
+# define N_DIR(t_file) !S_ISDIR(t_file->info.st_mode)
 
 /*
-**	-------STRUCTS-------
+**	--------------------------------
+**				STRUCTS
+**	---------------------------------
 */
 
 typedef	struct		s_file
@@ -47,24 +55,50 @@ typedef	struct		s_file
 	char			*path;
 	char			*name;
 	char			*full_path;
+	struct stat		info;
 	struct s_file	*next;
 }					t_file;
 
+/*
+**	--------------------------------
+**				GETTERS
+**	--------------------------------
+*/
+
 char				get_flags(char **av, char mask, int i, int j);
 t_file				*get_dirs(char **av, int i, int j);
+
+/*
+**	--------------------------------
+**				T_FILE
+**	--------------------------------
+*/
+
 t_file				*t_fileinit(char *param);
 t_file				*t_filedel(t_file *file);
 int					t_fileadd(t_file **apath, char *dir);
 void				t_filedelone(t_file **apath, int index);
-t_file				*t_filepushfront(t_file **apath, int index);
-void				sort_paths(t_file **paths);
-void				t_file_mergesort(t_file **apath, char flags);
 
-int					dash_onef(t_file *paths);
+/*
+**	--------------------------------
+**				DISPLAY
+**	--------------------------------
+*/
+
 int					ft_ls(t_file *paths, char flags);
 
 /*
-**	MANDATORY FLAGS
+**	--------------------------------
+**				SORT
+**	--------------------------------
+*/
+
+void				t_file_mergesort(t_file **apath, char flags, int type);
+
+/*
+**	--------------------------------
+**			MANDATORY FLAGS
+**	--------------------------------
 **	-R	Recursively list subdirectories encountered.
 **	-a	Include directory entries whose names begin with a dot (.).
 **	-l	List in long format. If the output is to a terminal, a total sum
@@ -73,10 +107,19 @@ int					ft_ls(t_file *paths, char flags);
 **		order or the oldest entries first
 **	-t	Sort by time modified (most recently modified first) before sorting
 **		the operands by lexicographical order
-**	OPTIONS FLAGS
+**	OPTIONAL FLAGS
 **	-f	Output is not sorted. This option turns on the -a option.
 **	-1	(The numeric digit 'one'.) Force output to be one entry per line.
 **		This is the default when output is not to a terminal.
 */
 
+/*
+**	-----------------------
+**			ARCHIVED
+**	-----------------------
+*/
+
+void				sort_paths(t_file **paths);
+int					dash_onef(t_file *paths);
+t_file				*t_filepushfront(t_file **apath, int index);
 #endif
