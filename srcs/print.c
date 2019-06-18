b25b27b4770cf980bb25fddef6aeeb26bf4db761
rpeepz <r.papagna.42@gmail.com>
2019-06-17 22:14:19 -0700

    long format output

--- srcs/print.c
+++ srcs/print.c
@@ -6,13 +6,13 @@
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/02 19:44:51 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/06/08 17:17:42 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/06/17 21:59:45 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
 
-static char		*ls_display(t_file *entry, char flags, int longest, int type)
+static void		ls_display(t_file *entry, char flags, int longest, int type)
 {
 	char		buf[PAGESIZE];
 	char		*tmp;
@@ -23,29 +23,25 @@ static char		*ls_display(t_file *entry, char flags, int longest, int type)
 	tmp = G_PATH(tmp, entry->full_path, entry->name);
 	IF_THEN(type == 1, ft_printf("\n%s:\n", tmp));
 	IF_THEN(type == 2, ft_printf("%s:\n", tmp) && (type = 1));
+	IF_THEN(flags & 0x10, norminette(&entry, flags, ""));
 	len = ft_sprintf(buf, "\0");
-	while (entry)
+	while (!(flags & 0x10) && entry)
 	{
 		IF_THEN_CONTINUE(!(flags & 0x4) && entry->name[0] == '.',
 			entry = entry->next);
 		if (flags & 0x1)
-			len += ft_sprintf(&buf[LEN(buf)], "%s\n", entry->name);
-		else if (flags & 0x10)
-			;
+			len += ft_sprintf(&buf[len], "%s\n", entry->name);
 		else
-			len += ft_sprintf(&buf[LEN(buf)], "%-*s", longest, entry->name);
+			len += ft_sprintf(&buf[len], "%-*s", longest, entry->name);
 		IF_THEN(!(entry = entry->next) || len > PAGESIZE - 255,
-			ft_printf("%s%s", buf, NOCOL) && (len = ft_sprintf(buf, "\0")));
+			ft_printf("%s", buf) && (len = ft_sprintf(buf, "\0")));
 	}
-	tmp = buf;
-	return (tmp);
 }
 
 int				print_contents(t_file *paths, char flags, int type)
 {
 	DIR				*dir;
 	t_file			*entry;
-	char			*buf;
 
 	entry = NULL;
 	dir = opendir(paths->name);
@@ -61,8 +57,7 @@ int				print_contents(t_file *paths, char flags, int type)
 			if (!(t_filedel(&entry)))
 				return (1);
 		t_file_mergesort(&entry, flags, 0);
-		IF_THEN((buf = ls_display(entry, flags, get_longest(entry, flags, 0),
-			type)), ft_printf("%s", buf));
+		ls_display(entry, flags, get_longest(entry, flags, 0), type);
 	}
 	if (!t_filedel(&entry) && paths->next)
 		return (print_contents(paths->next, flags, 1));
@@ -70,30 +65,27 @@ int				print_contents(t_file *paths, char flags, int type)
 	return (0);
 }
 
-int				print_first_files(t_file **apath, char flags, int longest)
+int				print_first_files(t_file **apath, char flgs, int lngst, int len)
 {
 	t_file	*paths;
 	char	buf[PAGESIZE];
 
 	paths = *apath;
-	buf[0] = '\0';
-	while (paths && N_DIR(paths))
+	len = ft_sprintf(buf, "\0");
+	IF_THEN(flgs & 0x10, norminette2(&paths, ""));
+	while (!(flgs & 0x10) && paths && N_DIR(paths))
 	{
 		opendir(paths->name);
 		if (!(ft_strcmp(strerror(errno), "No such file or directory")))
 			ft_printf("ls: %s: %s\n", paths->name, strerror(errno));
-		else if (flags & 0x1)
-			ft_sprintf(&buf[LEN(buf)], "%s\n", paths->name);
-		else if (flags & 0x10)
-			;
+		else if (flgs & 0x1)
+			len += ft_sprintf(&buf[len], "%s\n", paths->name);
 		else
-			ft_sprintf(&buf[LEN(buf)], "%-*s", longest, paths->name);
-		IF_THEN(paths->name, free(paths->name));
-		IF_THEN(paths->full_path, free(paths->full_path));
-		paths = paths->next;
+			len += ft_sprintf(&buf[len], "%-*s", lngst, paths->name);
+		ft_pipewrench("-s-s", paths->name, paths->full_path);
+		IF_THEN(!(paths = paths->next) || Y_DIR(paths) || len > PAGESIZE
+		- 255, ft_printf("%s", buf) && (len = ft_sprintf(buf, "\0")));
 	}
-	ft_printf("%s%c", buf, flags & 0x1 ? '\0' : '\n');
-	if (paths)
-		return (print_contents(paths, flags, 1));
+	IF_RETURN(paths, print_contents(paths, flgs, 1));
 	return (0);
 }
